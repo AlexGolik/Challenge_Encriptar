42328750355e476ea66508b52ee3320e1abef293
AlexGolik <alexgoblikon@gmail.com>
2024-07-16 14:27:13 -0500

    fixed: main.js, styles.css

--- Js/main.js
+++ Js/main.js
@@ -4,11 +4,10 @@ function encrypt(text, shift) {
     for (let i = 0; i < text.length; i++) {
         let charCode = text.charCodeAt(i);
         if (charCode >= 97 && charCode <= 122) {
-            // Solo para letras minúsculas
             let encryptedCharCode = ((charCode - 97 + shift) % 26) + 97;
             result += String.fromCharCode(encryptedCharCode);
         } else {
-            result += text.charAt(i); // Mantiene caracteres especiales sin cambios
+            result += text.charAt(i);
         }
     }
     return result;
@@ -20,11 +19,10 @@ function decrypt(encryptedText, shift) {
     for (let i = 0; i < encryptedText.length; i++) {
         let charCode = encryptedText.charCodeAt(i);
         if (charCode >= 97 && charCode <= 122) {
-            // Solo para letras minúsculas
             let decryptedCharCode = ((charCode - 97 - shift + 26) % 26) + 97;
             result += String.fromCharCode(decryptedCharCode);
         } else {
-            result += encryptedText.charAt(i); // Mantiene caracteres especiales sin cambios
+            result += encryptedText.charAt(i);
         }
     }
     return result;
@@ -33,7 +31,7 @@ function decrypt(encryptedText, shift) {
 // Función para encriptar el texto
 function encryptText() {
     let input = document.getElementById('input-text').value.toLowerCase(); // Convertimos a minúsculas
-    let shift = 7; // Puedes ajustar el desplazamiento como desees
+    let shift = 7;
     let encrypted = encrypt(input, shift);
     let encryptedTextArea = document.getElementById('encrypted-text');
     let copiar = document.getElementById('copiar');
@@ -44,32 +42,31 @@ function encryptText() {
 
     if (input.trim() !== '') {
         // Si hay texto en el input se muestra el texto encriptado y se oculta el placeholder de la derecha
-        encryptedTextArea.value = encrypted; // Mostramos el texto encriptado
-        encryptedTextArea.style.display = 'block'; // Mostramos el textarea encriptado
-        copiar.style.display = 'block'; // Mostramos el botón de cop
-        rightContainer.classList.add('encrypted'); // Añadimos la clase 'encrypted'
-        placeholderImg.style.display = 'none'; // Ocultamos el placeholder
-        placeholderH2.style.display = 'none'; // Ocultamos el placeholder
-        placeholderH5.style.display = 'none'; // Ocultamos el placeholder
+        encryptedTextArea.value = encrypted;
+        encryptedTextArea.style.display = 'block';
+        copiar.style.display = 'block';
+        rightContainer.classList.add('encrypted');
+        placeholderImg.style.display = 'none';
+        placeholderH2.style.display = 'none';
+        placeholderH5.style.display = 'none';
     } else {
         // Si no hay texto en el input se limpia el textarea y se muestra el placeholder
-        encryptedTextArea.value = ''; // Limpiamos el textarea
-        encryptedTextArea.style.display = 'none'; // Ocultamos el textarea encriptado
-        rightContainer.classList.remove('encrypted'); // Quitamos la clase 'encrypted'
-        placeholderImg.style.display = 'block'; // Mostramos el placeholder
-        placeholderH2.style.display = 'block'; // Mostramos el placeholder
-        placeholderH5.style.display = 'block'; // Mostramos el placeholder
+        encryptedTextArea.value = '';
+        encryptedTextArea.style.display = 'none';
+        rightContainer.classList.remove('encrypted');
+        placeholderImg.style.display = 'block';
+        placeholderH2.style.display = 'block';
+        placeholderH5.style.display = 'block';
     }
 }
 
 // Función para desencriptar el texto
 function decryptText() {
     let encryptedText = document.getElementById('encrypted-text').value;
-    let shift = 7; // Asegúrate de usar el mismo desplazamiento que en la encriptación
+    let shift = 7;
     let decrypted = decrypt(encryptedText, shift);
-    document.getElementById('encrypted-text').value = decrypted; // Mostramos el texto desencriptado en el input de texto
+    document.getElementById('encrypted-text').value = decrypted;
 }
-
 // Función para copiar el texto del textarea encriptado
 function copyText() {
     let encryptedText = document.getElementById('encrypted-text').value;
@@ -83,7 +80,7 @@ function copyText() {
             showMessage('Texto copiado: ' + encryptedText, 'success');
         })
         .catch((err) => {
-            // Manejar errores si no se puede copiar
+            // errores si no se puede copiar
             console.error('Error al copiar texto: ', err);
             showMessage(
                 'Error al copiar texto. Por favor, intenta nuevamente.',
@@ -102,11 +99,11 @@ function showMessage(message, type) {
     alertDiv.textContent = message;
 
     // Agregar el mensaje al contenedor
-    messageContainer.innerHTML = ''; // Limpiar mensajes anteriores
+    messageContainer.innerHTML = '';
     messageContainer.appendChild(alertDiv);
 
     // Ocultar el mensaje después de 3 segundos (3000 milisegundos)
     setTimeout(() => {
         messageContainer.innerHTML = '';
-    }, 3000); // 3 segundos
+    }, 3000);
 }
